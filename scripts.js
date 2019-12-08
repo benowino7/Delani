5f99149d5408129c99f754b5e6e0c5ca39f44a4d
benson7-coder <bensonowino7@gmail.com>
2019-12-08 15:13:43 +0300

    created a footer and social media icons and styled margin and position

--- scripts.js
+++ scripts.js
@@ -9,26 +9,26 @@
      $('p#p2').toggle();
 });
 });
-function _f() {
-    return document.forms['forms'];
-}
-_f()['btn'].addEventListener('click', function (f) {
-    f.preventDefault();
-    checkEmpty(name, email, text);
-});
-function checkEmpty(name, email, text) {
-    name = (document.forms[0][0]).value;
-    email = (document.forms[0][1]).value;
-    text = (document.forms[0][2]).value;
-    if (name == "" || email == "" || text == ""){
-        alert('please enter your details')
-    }
-else if (name == "") {
-        alert("please enter your name");
-        return 0;
-    }
+// function _f() {
+//     return document.forms['forms'];
+// }
+// _f()['btn'].addEventListener('click', function (f) {
+//     f.preventDefault();
+//     checkEmpty(name, email, text);
+// });
+// function checkEmpty(name, email, text) {
+//     name = (document.forms[0][0]).value;
+//     email = (document.forms[0][1]).value;
+//     text = (document.forms[0][2]).value;
+//     if (name == "" || email == "" || text == ""){
+//         alert('please enter your details')
+//     }
+// else if (name == "") {
+//         alert("please enter your name");
+//         return 0;
+//     }
 
 
 
 
-}
+// }
