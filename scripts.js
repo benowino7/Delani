ad787207dc79523a9df0897e77e8f2a12639cdf1
benson7-coder <bensonowino7@gmail.com>
2019-12-08 15:01:55 +0300

    created a footer and social media icons and styled margin and position

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-// $(document).ready(function(){
-//  $('img#img').click(function(toggle){
-//    $('p#p').toggle();
-//  });
-//  $('img#img1').click(function(toggle){
-//     $('p#p1').toggle();
-// });
-// $('img#img2').click(function(toggle){
-//     $('p#p2').toggle();
-// });
-// });
+ $(document).ready(function(){
+ $('img#img').click(function(toggle){
+    $('p#p').toggle();
+  });
+  $('img#img1').click(function(toggle){
+     $('p#p1').toggle();
+ });
+ $('img#img2').click(function(toggle){
+     $('p#p2').toggle();
+});
+});
 function _f() {
     return document.forms['forms'];
 }
