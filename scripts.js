5b39cd08b188da3b55c5fc806131de473eee29cd
benson7-coder <bensonowino7@gmail.com>
2019-12-08 14:05:18 +0300

    added toggle js and jquery function

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ $('img#img').click(function(toggle){
+   $('p#p').toggle();
+ });
+ $('img#img1').click(function(toggle){
+    $('p#p1').toggle();
+});
+$('img#img2').click(function(toggle){
+    $('p#p2').toggle();
+});
+
+});
